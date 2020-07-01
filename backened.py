d1b6b8b6a870a8c91fb408126d2426b69658d151
Aashi <mohita0987@gmail.com>
2020-07-01 12:39:41 +0530

    backened changes

--- backened.py
+++ backened.py
@@ -6,4 +6,17 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
     
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+    
+
+@app.route('/start/category=<string:num>/difficulty=<string:level>')
+def start(num,level):
+    return render_template('test.html',num=num,level=level)
+
+@app.route('/quiz/category=<string:num>/difficulty=<string:level>')
+def quiz(num,level):
+    return render_template('quiz.html',num=num,level=level)
 app.run(debug=True)
--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,43 +1,43 @@
+function start(num,level) {
+  sessionStorage.setItem('ques', JSON.stringify(new Array()))
+  url = `https://opentdb.com/api.php?amount=20&category=${num}&difficulty=${level}&type=multiple`
+  fetch(url).then(response => response.json()).then(data => {
+    console.log(data)
+    quiz(data)
+  })
+}
+function quiz(data) {
+  data.results.forEach((elem) => {
+    let que = elem.question
+    let correctAns = elem.correct_answer
+    let incorrectAns = elem.incorrect_answers
+    l = incorrectAns
+    l.push(correctAns)
+    let a = Math.floor(4 * Math.random())
+    let change = l[a]
+    l[3] = change
+    l[a] = correctAns
 
-sessionStorage.setItem('ques',JSON.stringify(new Array()))
-let url='https://opentdb.com/api.php?amount=20&category=17&difficulty=hard&type=multiple'
-fetch(url).then(response=>response.json()).then(data=>{
-
-  quiz(data)
-})
-
-function quiz(data){
-  data.results.forEach((elem,index)=>{
-    let opt=[1,2,3,4]
-    let que=elem.question
-    let correctAns=elem.correct_answer
-    let incorrectAns=elem.incorrect_answers
-    l=incorrectAns
-
-    let a=Math.ceil(3*Math.random())
-    let change=l[a]
-    l[3]=change
-    l[a]=correctAns
-
-    let set=document.querySelector('.swiper-wrapper')
-    let Obj=JSON.parse(sessionStorage.getItem('ques'))
-    let put={
-      ques:que,
-      options:l,
-      correct_answer:correctAns
+    let set = document.querySelector('.swiper-wrapper')
+    let Obj = JSON.parse(sessionStorage.getItem('ques'))
+    let put = {
+      ques: que,
+      options: l,
+      correct_answer: correctAns
     }
     Obj.push(put)
-    sessionStorage.setItem('ques',JSON.stringify(Obj))
-   
+    sessionStorage.setItem('ques', JSON.stringify(Obj))
+
   })
   displayQues()
 }
 
-function displayQues(){
-  let paper=sessionStorage.getItem('ques')
-  Obj=JSON.parse(paper)
-  Array.from(document.querySelector('.swiper-wrapper').children).forEach((elem,index)=>{
-    elem.innerHTML=`<div>${Obj[index].ques}</div>
+
+function displayQues() {
+  let paper = sessionStorage.getItem('ques')
+  Obj = JSON.parse(paper)
+  Array.from(document.querySelector('.swiper-wrapper').children).forEach((elem, index) => {
+    elem.innerHTML = `<div>${Obj[index]['ques']}</div>
    <div>
       <input type="radio" name="que${index}" value="${Obj[index].options[0]}">${Obj[index].options[0]}  
       <input type="radio" name="que${index}" value="${Obj[index].options[1]}">${Obj[index].options[1]} 
