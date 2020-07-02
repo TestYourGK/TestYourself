31eb0acf803483c7c900f3ce45e8ba0fca1db247
Aashi <mohita0987@gmail.com>
2020-07-02 11:44:04 +0530

    changes in quiz

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,9 +1,16 @@
-function start(num,level) {
-  sessionStorage.setItem('ques', JSON.stringify(new Array()))
-  url = `https://opentdb.com/api.php?amount=20&category=${num}&difficulty=${level}&type=multiple`
+let num =document.getElementById('num').innerText
+    let level =document.getElementById('level').innerText
+    start(num,level)
+
+
+function start(num,level){
+  url=`https://opentdb.com/api.php?amount=10&category=${num}&difficulty=${level}&type=multiple`
+  console.log(url)
   fetch(url).then(response => response.json()).then(data => {
     console.log(data)
+    sessionStorage.setItem('ques',JSON.stringify([]))
     quiz(data)
+
   })
 }
 function quiz(data) {
@@ -38,11 +45,11 @@ function displayQues() {
   Obj = JSON.parse(paper)
   Array.from(document.querySelector('.swiper-wrapper').children).forEach((elem, index) => {
     elem.innerHTML = `<div>${Obj[index]['ques']}</div>
-   <div>
-      <input type="radio" name="que${index}" value="${Obj[index].options[0]}">${Obj[index].options[0]}  
-      <input type="radio" name="que${index}" value="${Obj[index].options[1]}">${Obj[index].options[1]} 
-      <input type="radio" name="que${index}" value="${Obj[index].options[2]}">${Obj[index].options[2]} 
-      <input type="radio" name="que${index}" value="${Obj[index].options[3]}">${Obj[index].options[3]} 
+   <div class='options'>
+      <div><input type="radio" name="que${index}" value="${Obj[index].options[0]}">${Obj[index].options[0]} </div> 
+      <div><input type="radio" name="que${index}" value="${Obj[index].options[1]}">${Obj[index].options[1]} </div>
+      <div><input type="radio" name="que${index}" value="${Obj[index].options[2]}">${Obj[index].options[2]} </div>
+      <div><input type="radio" name="que${index}" value="${Obj[index].options[3]}">${Obj[index].options[3]} </div>
     </div>`
   })
 }
