d3b8227b44469d49778e17dc6e043dd20c615e90
Aashi <mohita0987@gmail.com>
2020-07-03 12:06:19 +0530

    small modifications

--- static/js/timer.js
+++ static/js/timer.js
@@ -6,7 +6,7 @@ let hr=now.getHours()
 let min=now.getMinutes()
 let seconds=now.getSeconds()
 let reqTime=new Date(new Date().getTime()+2700000)
-let m=44
+let m=29
 let s=59
 let myVar=setInterval(()=>{
     timer.innerText=`${(m>9)?''+m:'0'+m}:${(s>9)?''+s:'0'+s}`
